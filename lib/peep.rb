a60aeeb1783bf2c61f3c7d6697c74d978712a658
Somayeh <mail.somayeh@gmail.com>
2018-04-29 18:51:49 +0100

    Creating database and fetch the data from database

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,8 +1,9 @@
+require 'pg'
+
 class Peep
   def self.all
-    [
-      "I am learning Sinatra",
-      "I am practicing database"
-    ]
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec("SELECT * FROM peeps")
+    result.map { |peep| peep['message'] }
   end
 end
