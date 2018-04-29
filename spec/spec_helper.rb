bb33530fa40fae1af7df0b80f58de479ab8f509c
Somayeh <mail.somayeh@gmail.com>
2018-04-29 17:05:58 +0100

    Make capybara talk to sinatra

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the `app.rb` file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# Require all the testing gems
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# Tell Capybara to talk to BookmarkManager
+Capybara.app = BookmarkManager
 require 'simplecov'
 require 'simplecov-console'
 
@@ -7,6 +19,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
 
 RSpec.configure do |config|
   config.after(:suite) do
